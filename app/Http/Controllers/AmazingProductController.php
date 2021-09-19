f6e9d073a7a38b8580df2334b72b3d4886bf1c27
Sajjad <msajjad.shahrabi@gmail.com>
2021-09-19 17:38:36 +0430

    + Add controllers

---
+++ app/Http/Controllers/AmazingProductController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AmazingProduct;
+use Illuminate\Http\Request;
+
+class AmazingProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AmazingProduct  $amazingProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AmazingProduct $amazingProduct)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AmazingProduct  $amazingProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AmazingProduct $amazingProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\AmazingProduct  $amazingProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AmazingProduct $amazingProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AmazingProduct  $amazingProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AmazingProduct $amazingProduct)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PrimarySpecificationTitleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PrimarySpecificationTitle;
+use Illuminate\Http\Request;
+
+class PrimarySpecificationTitleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PrimarySpecificationTitle  $specification
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PrimarySpecificationTitle $specification)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PrimarySpecificationTitle  $specification
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PrimarySpecificationTitle $specification)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PrimarySpecificationTitle  $specification
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PrimarySpecificationTitle $specification)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PrimarySpecificationTitle  $specification
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PrimarySpecificationTitle $specification)
+    {
+        //
+    }
+}
