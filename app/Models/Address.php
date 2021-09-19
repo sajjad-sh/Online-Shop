1bd996bb5ba4cf9fe328e3b4cb9067e163247131
Sajjad <msajjad.shahrabi@gmail.com>
2021-09-20 01:58:46 +0430

    + Add Address model

---
+++ app/Models/Address.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'content',
+        'location'
+    ];
+
+    /**
+     * Get the user that owns the address.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the orders for the address.
+     */
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+}
