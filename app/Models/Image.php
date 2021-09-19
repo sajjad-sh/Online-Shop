edcb7c7f798dd8537f77e1d2f3c3dd51d290e3ca
Sajjad <msajjad.shahrabi@gmail.com>
2021-09-20 01:55:13 +0430

    + Add Image Product morphedByMany relationship + Add Image Category morphedByMany relationship

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -18,4 +18,20 @@ class Image extends Model
         'url',
         'is_primary'
     ];
+
+    /**
+     * Get all of the products that are assigned this image.
+     */
+    public function products()
+    {
+        return $this->morphedByMany(Product::class, 'imageable');
+    }
+
+    /**
+     * Get all of the categories that are assigned this image.
+     */
+    public function categories()
+    {
+        return $this->morphedByMany(Category::class, 'imageable');
+    }
 }
