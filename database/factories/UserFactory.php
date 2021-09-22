a4f9d15bc6a8fa66c8b1e37268100e82b5204d28
Sajjad <msajjad.shahrabi@gmail.com>
2021-09-22 17:28:19 +0330

    Update definition of UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,14 +20,23 @@ class UserFactory extends Factory
      *
      * @return array
      */
+
     public function definition()
     {
+
         return [
-            'name' => $this->faker->name(),
+            'cart_id' => $this->faker->randomNumber(),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
             'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->unique()->phoneNumber(),
+            'role' => $this->faker->numberBetween(0, 4),
+//            'latest_categories' => $this->faker->shuffleArray(),
+//            'latest_products' => $this->faker->shuffleArray(),
+//            'favorite_products' => $this->faker->shuffleArray(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'remember_token' => Str::random(10)
         ];
     }
 
