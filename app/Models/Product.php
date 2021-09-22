85cee291aea91fc4237c21fd67b3b9d9293d5c8b
Sajjad <msajjad.shahrabi@gmail.com>
2021-09-22 17:23:41 +0330

    + Add softdelete feature to Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
 
     /**
@@ -25,7 +26,6 @@ class Product extends Model
         'visits',
         'rate',
         'review',
-        'primary_specifications',
         'special_specifications',
         'status'
     ];
