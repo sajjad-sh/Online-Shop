93a9a0d22e321469efaf35831c39a44972bca4af
Sajjad <msajjad.shahrabi@gmail.com>
2021-09-22 17:19:53 +0330

    + Add admin.shop.index view

---
+++ resources/views/admin/shop/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title', 'پنل مدیریت')
+
+@section('content')
+
+    <h1>داشبورد مدیریت</h1><br>
+
+    <ul class="list-group">
+        <li class="list-group-item">لیست محصولات</li>
+        <li class="list-group-item">لیست دسته‌بندی‌ها</li>
+        <li class="list-group-item">لیست خصوصیات</li>
+        <li class="list-group-item">لیست نظرات</li>
+    </ul>
+
+@endsection
