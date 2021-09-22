f3610737c7acdfb261094b971195e515597212f8
Sajjad <msajjad.shahrabi@gmail.com>
2021-09-22 17:20:17 +0330

    + Add admin.index view

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('title', 'پنل مدیریت')
+
+@section('content')
+
+    <h1>داشبورد مدیریت</h1><br>
+
+    <ul class="list-group">
+        <li class="list-group-item">لیست کاربران</li>
+        <li class="list-group-item">لیست سفارشات</li>
+        <li class="list-group-item">لیست مالی</li>
+        <li class="list-group-item">لیست کدهای تخفیف</li>
+        <li class="list-group-item">مدیریت فروشگاه</li>
+        <li class="list-group-item">مدیریت سایت</li>
+    </ul>
+
+@endsection
