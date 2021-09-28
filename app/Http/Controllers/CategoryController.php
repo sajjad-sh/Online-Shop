e3a45cc77a911ad43829c0574ffa6ad9d9a0c879
Sajjad <msajjad.shahrabi@gmail.com>
2021-09-29 02:22:55 +0330

    Implement CRUD for CategoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,62 +2,65 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\View\View;
 
 class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\View\View
      */
     public function index()
     {
-        //
+        $categories = Category::all();
+
+        return view('admin.shop.categories.index')
+            ->with('categories', $categories);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return View
      */
     public function create()
     {
-        //
+        $categories = Category::all();
+
+        return view('admin.shop.categories.create')
+            ->with('categories', $categories);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(Request $request)
+    public function store(StoreCategoryRequest $request)
     {
-        //
-    }
+        Category::create($request->validated());
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Category $category)
-    {
-        //
+        return redirect()->route('admin.shop.categories.index');
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
+     * @return View
      */
     public function edit(Category $category)
     {
-        //
+        return view('admin.shop.categories.edit')
+            ->with('category', $category);
     }
 
     /**
@@ -65,21 +68,34 @@ class CategoryController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function update(Request $request, Category $category)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'slug' => ['required','min:2', 'max:50', 'regex:/^[A-Za-z0-9-]+$/', Rule::unique('categories')->ignore($category->id)],
+            'name' => 'required|min:2|max:50',
+            'parent_id' => 'integer',
+            'icon' => 'url',
+            'description' => 'text'
+        ]);
+
+        if($validator)
+            $category->update($request->all());
+
+        return back();
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        return back();
     }
 }
