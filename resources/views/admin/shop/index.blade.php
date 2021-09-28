beb158da921f2687c597037dc1d23666cc6cd1c9
Sajjad <msajjad.shahrabi@gmail.com>
2021-09-29 02:29:09 +0330

    + Add admin.shop.index view

--- resources/views/admin/shop/index.blade.php
+++ resources/views/admin/shop/index.blade.php
@@ -4,13 +4,15 @@
 
 @section('content')
 
+  <a href="{{route('admin.index')}}">برگشت به پنل مدیریت - صفحه اصلی</a>
+
     <h1>داشبورد مدیریت</h1><br>
 
     <ul class="list-group">
-        <li class="list-group-item">لیست محصولات</li>
-        <li class="list-group-item">لیست دسته‌بندی‌ها</li>
-        <li class="list-group-item">لیست خصوصیات</li>
-        <li class="list-group-item">لیست نظرات</li>
+        <li class="list-group-item"><a href="{{route('admin.shop.products.index')}}">لیست محصولات</a></li>
+        <li class="list-group-item"><a href="{{route('admin.shop.categories.index')}}">لیست دسته‌بندی‌ها</a></li>
+        <li class="list-group-item"><a href="{{route('admin.shop.specifications.index')}}">لیست خصوصیات</a></li>
+        <li class="list-group-item"><a href="#">لیست نظرات</a></li>
     </ul>
 
 @endsection
