e5735b3d36dccdf567d777599c88e6d717dee948
Sajjad <msajjad.shahrabi@gmail.com>
2021-09-29 21:14:54 +0330

    + Add admin.shop.specifications.edit view

--- resources/views/admin/shop/specifications/edit.blade.php
+++ resources/views/admin/shop/specifications/edit.blade.php
@@ -13,7 +13,7 @@
         <div class="col">
           <div class="card">
             <form class="form-horizontal" method="post"
-                  action="{{route('admin.shop.specifications.update', $primary_specification_value->id)}}">
+                  action="{{route('admin.shop.specifications.update', $primary_specification_value)}}">
               @csrf
               @method('PATCH')
               <div class="card-body">
@@ -27,21 +27,6 @@
                 @endif
                 <div class="form-group row">
                   <br>
-
-                  <div class="col-sm-9">
-                    <label for="title">عنوان</label>
-                    <select name="title" class="form-select" aria-label="Default select example">
-                      <option selected id="title"
-                              >{{$primary_specification_value->primary_specification_title->title}}</option>
-                      @foreach($primary_specification_titles as $primary_specification_title)
-                        @if($primary_specification_title->title == $primary_specification_value->primary_specification_title->title)
-                          @continue
-                        @endif
-                        <option
-                                value="{{$primary_specification_title->id}}">{{$primary_specification_title->title}}</option>
-                      @endforeach
-                    </select>
-                  </div>
 
                   <div class="col-sm-9">
                     <label for="value">مقدار</label>
