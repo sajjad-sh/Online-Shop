a6615dd3651899fd80a4eac3b6da1fc0742a0307
Sajjad <msajjad.shahrabi@gmail.com>
2021-10-01 18:30:03 +0330

    Implement creat, store and edit methods.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
 use App\Models\PrimarySpecificationTitle;
+use App\Models\PrimarySpecificationValue;
 use App\Models\Product;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -26,22 +29,49 @@ class ProductController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return view
      */
     public function create()
     {
-        //
+        $titles = PrimarySpecificationTitle::all();
+        $values = PrimarySpecificationValue::all();
+
+        return view('admin.shop.products.create')
+            ->with('titles', $titles)
+            ->with('values', $values);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return RedirectResponse
      */
-    public function store(Request $request)
+    public function store(StoreProductRequest $request)
     {
-        //
+
+        $special_specifications = json_encode(
+            array_combine($request->validated()['s_titles'], $request->validated()['s_values'])
+        );
+
+        $product = Product::create([
+            'fa_title' => $request->validated()['fa_title'],
+            'en_title' => $request->validated()['en_title'],
+            'price' => $request->validated()['price'],
+            'inventory' =>  $request->validated()['inventory'],
+            'review' =>  $request->validated()['review'],
+            'status' =>  $request->validated()['status'],
+            'special_specifications' =>  $special_specifications,
+        ]);
+
+        $product->amazing()->associate($request->validated()['amazing_id']);
+
+        $product->save();
+
+        foreach ($request->validated()['p_values'] as $p_value)
+            $product->primary_specification_values()->attach($p_value);
+
+        return redirect()->route('admin.shop.products.index');
     }
 
     /**
@@ -61,18 +91,43 @@ class ProductController extends Controller
      * @param  \App\Models\Product  $product
      * @return View
      */
-    public function edit($id)
+    public function edit(Product $product)
     {
-        $product = Product::findOrFail($id);
-        $special_specifications = json_decode($product->special_specifications, true);
 
-        $primary_specification_titles = PrimarySpecificationTitle::with(['primary_specification_values'])
-            ->get();
+        $all_titles = PrimarySpecificationTitle::all();
+        $all_values = PrimarySpecificationValue::all();
+
+//        foreach ($all_titles as $title)
+//            echo $title->title.'<br>';
+
+//        dd($product->primary_specification_values[0]->value);
+//        dd($product->primary_specification_values[0]->primary_specification_title->title);
+
+//        $p_values = $product->primary_specification_values;
+//        $p_titles = $product->primary_specification_values->primary_specification_title;
+
+
+//        $p_values = [];
+
+//        dd($product->primary_specification_values[1]->primary_specification_title->id);
+
+//        foreach ($product->primary_specification_values as $p_value) {
+//            $p_values[] = $p_value->pivot->spec_id;
+//            $p_titles[] = $p_value->primary_specification_title->id;
+//        }
+
+//        dd(array_combine($p_titles, $p_values));
+
+        $s_values = json_decode($product->special_specifications, true);
+//        dd($s_values);
 
         return view('admin.shop.products.edit')
             ->with('product', $product)
-            ->with('special_specifications', $special_specifications)
-            ->with('primary_specification_titles', $primary_specification_titles);
+            ->with('all_titles', $all_titles)
+            ->with('all_values', $all_values)
+//            ->with('p_titles', $p_titles)
+//            ->with('p_values', $p_values)
+            ->with('s_values', $s_values);
     }
 
     /**
@@ -80,7 +135,7 @@ class ProductController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\RedirectResponse
+     * @return RedirectResponse
      */
     public function update(UpdateProductRequest $request, Product $product)
     {
