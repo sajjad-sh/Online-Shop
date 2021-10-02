e2a76eb3befe8bbc743db7578a3db3a07748bc27
Sajjad <msajjad.shahrabi@gmail.com>
2021-10-02 13:48:06 +0330

    + Add alt column to images

---
+++ database/migrations/2021_10_02_131728_add_alt_to_images.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAltToImages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->text('alt')->after('url')->default('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->dropColumn('alt');
+        });
+    }
+}
