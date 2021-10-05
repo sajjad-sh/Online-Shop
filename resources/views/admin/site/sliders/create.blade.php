27b88665b180a61cacbb0a56000c00bc40fd2604
Sajjad <msajjad.shahrabi@gmail.com>
2021-10-05 13:41:37 +0330

    Some changes

--- resources/views/admin/site/sliders/create.blade.php
+++ resources/views/admin/site/sliders/create.blade.php
@@ -14,21 +14,30 @@
   <form action="{{route('admin.site.sliders.store')}}" method="post" enctype="multipart/form-data">
     @csrf
     <div class="mb-3">
-      <label for="alt" class="form-label">متن جایگزین فایل</label>
-      <input name="alt" class="form-control" type="text" aria-label="default input example">
-      <br>
+      <label for="alt" class="form-label">متن جایگزین عکس</label>
+      <input name="alt" class="form-control" type="text" aria-label="default input example" value="{{old('alt')}}">
+
+      <label for="link" class="form-label">لینک</label>
+      <input dir="ltr" name="link" class="form-control" type="text" aria-label="default input example" value="{{old('link')}}">
+
+      <label for="title" class="form-label">عنوان اسلایدر</label>
+      <input name="title" class="form-control" type="text" aria-label="default input example" value="{{old('title')}}">
+
+      <label for="subtitle" class="form-label">زیر عنوان اسلایدر</label>
+      <input name="subtitle" class="form-control" type="text" aria-label="default input example" value="{{old('subtitle')}}">
+
       <div class="form-floating">
-        <textarea name="extra" class="form-control" placeholder="توضیحات بیشتر" id="extra"
-                  style="height: 100px"></textarea>
+        <textarea name="description" class="form-control" placeholder="توضیحات بیشتر" id="extra"
+                  style="height: 100px">{{old('description')}}</textarea>
         <label for="extra">توضیحات</label>
       </div>
 
-      <label for="cfile" class="form-label">انتخاب فایل</label>
-      <input name="file" class="form-control" type="file" id="slider-file">
+      <label for="file" class="form-label">انتخاب فایل</label>
+      <input name="file" class="form-control" type="file" id="slider-file" value="{{old('file')}}">
 
       <br>
 
-        <select name="category_id" class="form-select" id="floatingSelect" aria-label="Floating label select example">
+        <select name="category_id" class="form-select" id="floatingSelect" aria-label="Floating label select example" >
           <option selected>انتخاب دسته بندی</option>
           <option value="0">صفحه اصلی</option>
 
