9a6f34c916fdc47444b3819dd9812acf800573fb
Sajjad <msajjad.shahrabi@gmail.com>
2021-10-06 21:49:44 +0330

    + Add description and slug properties to fillable + Add getBrandAttribute for fetch brand of product. + Add getPrimaryAttributesAttribute for fetch key value attributes (primary technical specifications) of product. + Add getSecondaryAttributesAttribute for fetch key value attributes (secondary technical specifications) of product.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -20,13 +20,14 @@ class Product extends Model
     protected $fillable = [
         'fa_title',
         'en_title',
+        'description',
+        'slug',
         'price',
         'inventory',
         'sales',
         'visits',
         'rate',
         'review',
-        'special_specifications',
         'status'
     ];
 
@@ -73,11 +74,11 @@ class Product extends Model
     }
 
     /**
-     * The primary specification values that belong to the product.
+     * The attribute values that belong to the product.
      */
-    public function primary_specification_values()
+    public function att_values()
     {
-        return $this->belongsToMany(PrimarySpecificationValue::class, 'product_spec', 'product_id', 'spec_id')->withTimestamps();
+        return $this->belongsToMany(AttValue::class, 'att_product', 'product_id', 'att_id')->withTimestamps();
     }
 
     public static function calculateDiscount($type, $amount, $price)
@@ -86,5 +87,53 @@ class Product extends Model
             return $price - ($amount/100 * $price);
 
         return $price - $amount;
+    }
+
+    # TODO: use accessor for user defined function and instead of query in blade
+
+    /**
+     * a query for fetch brand of product.
+     */
+    public function getBrandAttribute()
+    {
+        foreach ($this->att_values as $att_value) {
+            if(AttTitle::find($att_value->att_title_id)->key == 'brand')
+                return $att_value->value;
+        }
+        return false;
+    }
+
+    /**
+     * a query for fetch key value primary attributes (primary technical specifications) of product.
+     */
+    public function getPrimaryAttributesAttribute()
+    {
+        $titles = [];
+        $values = [];
+        foreach ($this->att_values as $att_value) {
+            if (AttTitle::find($att_value->att_title_id)->key == 'brand' or AttTitle::find($att_value->att_title_id)->is_primary == 0)
+                continue;
+            $titles[] = AttTitle::find($att_value->att_title_id)->title;
+            $values[] = $att_value->value;
+        }
+
+        return array_combine($titles, $values);
+    }
+
+    /**
+     * a query for fetch key value secondary attributes (secondary technical specifications) of product.
+     */
+    public function getSecondaryAttributesAttribute()
+    {
+        $titles = [];
+        $values = [];
+        foreach ($this->att_values as $att_value) {
+            if (AttTitle::find($att_value->att_title_id)->key == 'brand' or AttTitle::find($att_value->att_title_id)->is_primary == 1)
+                continue;
+            $titles[] = AttTitle::find($att_value->att_title_id)->title;
+            $values[] = $att_value->value;
+        }
+
+        return array_combine($titles, $values);
     }
 }
