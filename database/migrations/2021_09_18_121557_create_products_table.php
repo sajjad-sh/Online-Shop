d020838cfd3215be512acebec43d170f3434a998
Sajjad <msajjad.shahrabi@gmail.com>
2021-10-06 20:07:20 +0330

    + Description, slug and SoftDelete to create_products_table migration

--- database/migrations/2021_09_18_121557_create_products_table.php
+++ database/migrations/2021_09_18_121557_create_products_table.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Models\Amazing;
-use App\Models\PrimarySpecificationValue;
+use App\Models\AttValue;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -21,16 +21,18 @@ class CreateProductsTable extends Migration
             $table->foreignIdFor(Amazing::class)->nullable();
             $table->string('fa_title');
             $table->string('en_title');
+            $table->text('description')->nullable();
+            $table->string('slug');
             $table->unsignedDouble('price')->default(0);
             $table->unsignedInteger('inventory')->default(0);
             $table->unsignedInteger('sales')->default(0);
             $table->unsignedInteger('visits')->default(0);
             $table->unsignedInteger('rate')->default(0);
             $table->text('review')->nullable();
-            $table->json('special_specifications')->nullable();
             $table->unsignedTinyInteger('status')->default(1);
 
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
