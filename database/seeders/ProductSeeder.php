ea4f7c90d0c772d0a9886d4acbb2ae566c62aee0
Sajjad <msajjad.shahrabi@gmail.com>
2021-10-06 20:19:08 +0330

    + Add some product with categories

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
 
@@ -15,5 +16,12 @@ class ProductSeeder extends Seeder
     public function run()
     {
         Product::factory(50)->create();
+        $categoires = Category::all();
+
+        Product::all()->each(function ($product) use ($categoires) {
+            $product->categories()->attach(
+                $categoires->random(rand(1, 43))->pluck('id')->toArray()
+            );
+        });
     }
 }
