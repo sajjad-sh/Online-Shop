752f1cc72bc6aa87abf20d6e2f96d820ae2a6c45
Sajjad <msajjad.shahrabi@gmail.com>
2021-10-10 12:34:13 +0330

    + Add getFullNameAttribute accessor

--- app/Models/User.php
+++ app/Models/User.php
@@ -82,4 +82,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Address::class);
     }
+
+    public function getFullNameAttribute()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
 }
